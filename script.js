070a6352daaeca9ff3ef0d7fb4b92fb8d0159254
nosdliw <acamedhf@gmail.com.br>
2023-03-19 21:54:51 -0300

    semana2

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 const item = {
-    nome: ("Leão").toUpperCase,
+    nome: ("Leão").toUpperCase(),
     nomeCientifico:("Panthera leo"),
     tempoMediodeVida: (15),
     classe: ("Mamífero"),
-    ambiente: ("Savana"),
+    habitos: ("Vivem em bandos de até 30 leões", "Quando ficam velhos, feridos ou doentes, os leões se isolam de seu bando", 
+    "Os machos lutam entre si para disputar a fêmea e vence quem for o mais forte"),
     estáExtinto: !true,
 };
 const item2 = {
-    nome: ("Leão marinho").toUpperCase,
+    nome: ("Leão marinho").toUpperCase(),
     nomeCientifico: ("Otariinae"),
     tempoMediodeVida: (25),
     classe: ("Mamífero"),
-    ambiente: ("Marinho"),
+    habitos: ("O leão-marinho é uma espécie que habitualmente vive em profundidades menores que 50m",
+     "Distribui-se pela região costeira de ambos os lados da América do Sul, agrupando-se em locais rochosos.",
+    "pode atingir cerca de 300 kg"),
     estáExtinto: !true,
 };
 const item3 = {
-    nome: ("Mico leão dourado").toUpperCase,
+    nome: ("Mico leão dourado").toUpperCase(),
     nomeCientifico: ("Leontopithecus rosalia"),
     tempoMediodeVida: (22),
     classe: ("Mamífero"),
-    ambiente: ("Mata Atlântica"),
+    habitos: ("Costumam viver em grupos de 2-8 indivíduos", "Emitem muitos sons como gritos, trinos, assovios e choros e longos chamados similares de aves", 
+    "Dorme em buracos ocos de árvores abandonados por outras espécies"),
     estáExtinto: !true,
 };
 
-let soma = (item.tempoMediodeVida + item2.tempoMediodeVida + item3.tempoMediodeVida);
-let media = `A média de vida entre os leões é ${soma / 3}`;
+// let soma = (item.tempoMediodeVida + item2.tempoMediodeVida + item3.tempoMediodeVida);
+// let media = `A média de vida entre os leões é ${soma / 3}`;
 
-console.log(media);
-console.log(item.estáExtinto && item2.estáExtinto && item3.estáExtinto);
+// console.log(media);
+// console.log(item.estáExtinto && item2.estáExtinto && item3.estáExtinto);
 
-
+console.log(item, item2, item3);
